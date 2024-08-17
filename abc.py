0e4f238c42ae6a172b7b9cb1db1246f729d3e30c
Sharath0409 <42493323+Sharath0409@users.noreply.github.com>
2024-08-17 19:42:14 +0530

    Demo file upload
    This is done to understand on how to upload files to github

---
+++ abc.py
@@ -0,0 +1,1 @@
+print ("Get Shit Done")
---
+++ demo.py
@@ -0,0 +1,10 @@
+print ("hello world")
+print (1+2)
+print ("1+2")
+a=10
+if (a==1):
+    print ("a is 1")
+elif (a==2):
+    print ("a is 2")
+else:
+    print ("a is greater than 2")
